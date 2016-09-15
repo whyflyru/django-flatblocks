91fcf17af289b3be004a2aaa97acbc1b4a3d94b4
Adrian Tangochin <tangochin@yandex.ru>
2016-09-16 03:14:54 +0500

    Move subdomain in list display

--- flatblocks/admin.py
+++ flatblocks/admin.py
@@ -4,7 +4,7 @@ from flatblocks.models import FlatBlock
 
 class FlatBlockAdmin(admin.ModelAdmin):
     ordering = ['slug', ]
-    list_display = ('slug', 'header', 'content', 'subdomain')
+    list_display = ('slug', 'header', 'subdomain', 'content')
     search_fields = ('slug', 'header', 'content', 'subdomain')
 
 admin.site.register(FlatBlock, FlatBlockAdmin)
